e00593927c65f8c39d5693ff119bfc8a87718fb6
Pavlo-py <pasanlez@gmail.com>
2025-10-01 12:41:22 +0100

    Class Group

--- src/org/ulpgc/is1/model/Group.java
+++ src/org/ulpgc/is1/model/Group.java
@@ -1,5 +1,20 @@
 package org.ulpgc.is1.model;
 
+import java.util.List;
+
 public class Group {
 
+    public final  String name;
+    public List<Contact>contacts;
+
+    public Group(String name) {
+        this.name = name;
+    }
+    public void addMember(Contact contact) {
+        contacts.add(contact);
+    }
+
+    public void removeMember(Contact contact) {
+        contacts.remove(contact);
+    }
 }
